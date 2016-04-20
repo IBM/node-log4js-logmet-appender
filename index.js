216f43d6b428cafe2bfd49d9cf031df1c70a5f97
hermanba <hermanba@ca.ibm.com>
2016-04-20 17:28:55 -0400

    Cleanup

--- index.js
+++ index.js
@@ -23,13 +23,10 @@ function appender(level, options) {
 
             const LogmetClient = require('@alchemy-kms/logmet-client').LogmetClient;
 
-            const group_id = options.group_id;
             const logging_token = options.logging_token;
             const space_id = options.space_id;
             const logging_host = options.logging_host;
-            const metrics_host = options.metrics_host;
             const logging_port = options.logging_port;
-            const metrics_port = options.metrics_port;
              // 
             const logging_client = LogmetClient.getLoggingClient(
                 null,                               //  client options
@@ -44,48 +41,13 @@ function appender(level, options) {
                 100                                 //  queue size
             );
 
-            const metrics_client = LogmetClient.getMetricsClient(
-                null,
-                metrics_host,
-                metrics_port,
-                space_id,
-                5,
-                2,
-                space_id,
-                logging_token,
-                false,
-                100
-            );
-
-            const metric_type = 'otc-api-test-local-service';
-            const low_value = 5;
-            const high_value = 100;
-            const metrics = [];
-
-            for (var sample = 0; sample < 10; sample++) {
-                setTimeout(function () {
-                    let value = Math.floor(Math.random() * (high_value - low_value) + low_value);
-
-                    let new_metric = new Map();
-                    new_metric.set('metric_path', [group_id, metric_type, 'count']);
-                    metrics.push(new_metric);
-                }, sample * 30000);
-            }
-
-            metrics_client.addMetricMessages(metrics);
-
             const message = log.data.join(' | ');
             const log_entry = new Map();
-            log_entry.set('component', options.group_id);
-            log_entry.set('type', metric_type);
-            log_entry.set('line', 'Life is good...');
+            log_entry.set('component', options.component);
+            log_entry.set('type', log.categoryName);
             log_entry.set('message', message);
 
-            const logs = [];
-
-            for (var msg_count = 0; msg_count < 10; msg_count++) {
-                logs.push(new Map(log_entry));
-            }
+            logging_client.addLogMessages([new Map(log_entry)]);
         }
     };
 };
@@ -94,13 +56,11 @@ function configure(config) {
 	if (process.env.log4js_logmet_enabled !== 'true') return function() {};
 
     const options = {
-        group_id: process.env.log4js_logmet_group_id || config.options && config.options.group_id,
+        component: process.env.log4js_logmet_component || config.options && config.options.component,
         logging_token: process.env.log4js_logmet_logging_token || config.options && config.options.logging_token,
         space_id: process.env.log4js_logmet_space_id || config.options && config.options.space_id,
         logging_host: process.env.log4js_logmet_logging_host || config.options && config.options.logging_host,
-        metrics_host: process.env.log4js_logmet_metrics_host || config.options && config.options.metrics_host,
         logging_port: process.env.log4js_logmet_logging_port || config.options && config.options.logging_port,
-        metrics_port: process.env.log4js_logmet_metrics_port || config.options && config.options.metrics_port
     };
 
     var optionsInvalid = false;
