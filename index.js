12a8346753e3b1ba89948595b26159cf69d89b54
hermanba <hermanba@ca.ibm.com>
2016-04-22 17:15:26 -0400

    Prototype moving off logmet-client

--- index.js
+++ index.js
@@ -9,48 +9,269 @@
 'use strict';
 
 var log4js = require('log4js'),
-    util = require('util');
+    util = require('util'),
+    logmetConnection = require('./lib/logmet-connection-singleton'),
+    tls = require('tls'),
+    _ = require('lodash');
 
 module.exports = {
     appender: appender,
     configure: configure
 };
 
-function appender(level, options) {
+function appender(level, options, tlsOpts) {
     return function(log) {
-        if (log && log.level && log.level.level && log.level.level >= level &&
-            log.startTime && log.data && log.data.length > 0) {
-
-            const LogmetClient = require('@alchemy-kms/logmet-client').LogmetClient;
-
-            const logging_token = options.logging_token;
-            const space_id = options.space_id;
-            const logging_host = options.logging_host;
-            const logging_port = options.logging_port;
-             // 
-            const logging_client = LogmetClient.getLoggingClient(
-                null,                               //  client options
-                logging_host,
-                logging_port,
-                space_id,                           //  space id
-                5,                                  //  batch send period in seconds
-                1,                                  //  batch send count
-                space_id,                           //  tenant id
-                logging_token,                      //  password
-                false,                              //  is super tenant
-                100                                 //  queue size
-            );
-
-            const message = log.data.join(' | ');
-            const log_entry = new Map();
-            log_entry.set('component', options.component);
-            log_entry.set('type', log.categoryName);
-            log_entry.set('message', message);
-
-            logging_client.addLogMessages([new Map(log_entry)]);
+        if (!logmetConnection.connection && !logmetConnection.connecting) {
+            logmetConnection.connecting = true;
+            logmetConnection.connection = tls.connect(tlsOpts, connected.bind(this, log, options));
+            logmetConnection.connection.setEncoding('utf8');
+            logmetConnection.connection.on('error', function(err) {
+                console.error('error in connection. Error: ' + JSON.stringify(err, null, 2));
+                logmetConnection.connection = null;
+            });
+            logmetConnection.connection.on('close', function(err) {
+                console.warn('Connection closed. Error: ' + JSON.stringify(err, null, 2));
+                logmetConnection.connection = null;
+            });
+            logmetConnection.connection.on('end', function(err) {
+                console.warn('Connection ended. Error: ' + JSON.stringify(err, null, 2));
+                logmetConnection.connection = null;
+            });
         }
+        else {
+            logMessage(log, options);
+        }
+       
+
+
     };
 };
+
+function connected(message, options) {
+    logmetConnection.connecting = false;
+    _identify(function(err) {
+        if (err) {
+            console.error('Failed to send identity to Logmet: ' + JSON.stringify(err, null, 2));
+            logmetConnection.connection = null;
+        }
+        else {
+            _authenticate(options, function() {
+                logMessage(message, options);
+            });
+            
+        }
+    });
+};
+
+function formatMessage(message) {
+    const buffer_size = 2                       //  '1W'
+        + 4                                     //  Number of messages, 32 Uint BE
+        + ((2 + 4 + 4) * 1)       //  Msg delimiter + sequence + msg #s
+        + message.length;
+
+
+        var payload = new Buffer(buffer_size);
+        var offset = 0;
+
+        payload.write(logmetConnection.WINDOW_DELIMITER, offset, logmetConnection.WINDOW_DELIMITER.length, 'utf8');
+        offset += logmetConnection.WINDOW_DELIMITER.length;
+
+        payload.writeUInt32BE(1, offset);
+        offset += 4;
+
+        var message_delimiter = logmetConnection.LOG_DELIMITER;
+
+        payload.write(message_delimiter, offset, message_delimiter.length, 'utf8');
+        offset += message_delimiter.length;
+
+        payload.writeUInt32BE(logmetConnection.sequence, offset);
+        offset += 4;
+
+
+        payload.write(message.toString(), offset, message.length, 'utf8');
+
+        offset += message.length;
+
+        logmetConnection.sequence += 1;
+        return payload;
+};
+
+function logMessage(log, options) {
+    if (!logmetConnection.connection) {
+        return setTimeout(logMessage.bind(this, log, options), 100);
+    }
+    var logData = log.data.join(' | ');
+    const log_entry = new Map();
+    log_entry.set('component', options.component);
+    log_entry.set('type', log.categoryName);
+    log_entry.set('message', logData);
+
+    var logBuffer = _formatToLogBuffer(log_entry);
+
+    var logWhitelist = process.env.log4js_syslog_appender_whitelist;
+    var categoriesToSend = logWhitelist && logWhitelist.split(',');
+
+    if (logWhitelist && categoriesToSend.indexOf(log.categoryName) === -1) return;
+
+    var formattedMessage = formatMessage(logBuffer);
+
+    if (isOpen()) {
+            logmetConnection.connection.pause();
+            logmetConnection.connection.write(formattedMessage, 'utf8', function onSendMessagesWriteCallback() {
+                    console.log('Messages sent to Logmet...' + formattedMessage);
+
+                    logmetConnection.connection.once('data', function handleSendMessagesReply(reply) {
+
+                        console.log('Received data from Logmet [' + reply + ']');
+
+                        if (reply.slice(0, logmetConnection.SUCCESS.length) === (logmetConnection.SUCCESS)) {
+                            var replyBuffer = new Buffer(reply);
+                            console.log('Logmet received '
+                                + replyBuffer.readUInt32BE(logmetConnection.SUCCESS.length, 6) + ' messages.');
+                            console.log('message sent');
+                            } else {
+                                console.error('Logmet rejected messages - failed to send messages.');
+                        }
+                    });
+            });
+            logmetConnection.connection.resume();
+        }
+};
+
+function _formatToLogBuffer(log_data) {
+    const buffer_size = sizeof_map(log_data)
+        + 4                      //  1 unsigned int for message size
+        + log_data.size * 8;     //  2 unsigned ints for each key and value pair (their size)
+
+    const log_message = new Buffer(buffer_size);
+    let offset = 0;
+
+    //  Write the number of pairs
+    log_message.writeUInt32BE(log_data.size, offset);
+    offset += 4;    //  skip 4 bytes for the UInt32
+
+    log_data.forEach(function processKeyValuePair(value, key) {
+        //key = _.toString(key).toLowerCase();
+        key = _.toString(key);
+        value = _.toString(value);
+
+        log_message.writeUInt32BE(key.length, offset);
+        offset += 4;
+
+        log_message.write(key, offset, key.length, 'utf8');
+        offset += key.length;
+
+        log_message.writeUInt32BE(value.length, offset);
+        offset += 4;
+
+        log_message.write(value, offset, value.length, 'utf8');
+        offset += value.length;
+    });
+
+    return log_message;
+}
+
+function sizeof_map(map) {
+    if ( !(map instanceof Map) ) {
+        throw new TypeError('object must be a Map');
+    }
+
+    let size = 0;
+
+    map.forEach(function processKeyValuePair(value, key) {
+        size += _.toString(key).length;
+        size += _.toString(value).length;
+    });
+
+    return size;
+}
+
+function isOpen() {
+    return logmetConnection.connection && logmetConnection.connection.readyState === 'open';
+}
+
+
+var _identify = function(on_identify_callback) {
+    if (isOpen()) {
+        const IDENTIFIER_PREFIX = '1I';
+        const identifier = logmetConnection.connection.localAddress;
+        const identification_message = new Buffer(IDENTIFIER_PREFIX.length + 1 + identifier.length);
+
+        let offset = 0;
+        identification_message.write(IDENTIFIER_PREFIX, offset, IDENTIFIER_PREFIX.length, 'utf8');
+        offset += IDENTIFIER_PREFIX.length;
+
+        identification_message.writeUInt8(identifier.length, offset);
+        offset += 1;
+
+        identification_message.write(identifier, offset, identifier.length, 'utf8');
+
+        logmetConnection.connection.write(identification_message, 'utf8');
+
+        on_identify_callback(null);
+    } else {
+        console.warn('Socket connection to logmet closed while attempting to identify.');
+
+        on_identify_callback(new Error('Socket connection is closed, unable to identify'));
+    }
+};
+
+var _authenticate = function(options, on_authenticate_callback) {
+    if (isOpen()) {
+        var tenant_type = '2T'; // not a super tenant.
+        var tenant_id = options.space_id;
+        var tenant_password = options.logging_token;
+
+        var authentication_message = new Buffer(
+            tenant_type.length
+            + tenant_id.length + 1
+            + tenant_password.length + 1
+            );
+
+        let offset = 0;
+
+        authentication_message.write(tenant_type, offset, tenant_type.length, 'utf8');
+        offset += tenant_type.length;
+
+        authentication_message.writeUInt8(tenant_id.length, offset);
+        offset += 1;
+
+        authentication_message.write(tenant_id, offset,
+            tenant_id.length, 'utf8');
+        offset += tenant_id.length;
+
+        authentication_message.writeUInt8(tenant_password.length, offset);
+        offset += 1;
+
+        authentication_message.write(tenant_password, offset,
+            tenant_password.length, 'utf8');
+
+        logmetConnection.connection.write(authentication_message, 'utf8', function onAuthenticationWriteCallback() {
+            logmetConnection.connection.once('data', function handleAuthenticationReply(reply) {
+                    if (reply.slice(0, 2) === logmetConnection.SUCCESS) {
+                        logmetConnection.sequence = 1;
+
+                        console.log('Authenticated...', reply);
+
+                        on_authenticate_callback();
+                    } else {
+                        console.error('Invalid tenant authentication, check tenant id or logging token');
+
+                        on_authenticate_callback(new Error('Failed to authenticate'));
+                    }
+            });
+        });
+    }
+    else {
+        console.warn('Socket closed while attempting to authenticate.');
+
+        on_authenticate_callback(new Error('Socket connection is closed, unable to authenticate'));
+    
+    }
+};
+
+
+
 
 function configure(config) {
 	if (process.env.log4js_logmet_enabled !== 'true') return function() {};
@@ -58,9 +279,14 @@ function configure(config) {
     const options = {
         component: process.env.log4js_logmet_component || config.options && config.options.component,
         logging_token: process.env.log4js_logmet_logging_token || config.options && config.options.logging_token,
-        space_id: process.env.log4js_logmet_space_id || config.options && config.options.space_id,
-        logging_host: process.env.log4js_logmet_logging_host || config.options && config.options.logging_host,
-        logging_port: process.env.log4js_logmet_logging_port || config.options && config.options.logging_port,
+        space_id: process.env.log4js_logmet_space_id || config.options && config.options.space_id
+    };
+
+    const tlsOpts = {
+        host: process.env.log4js_logmet_logging_host || config.options && config.options.logging_host,
+        port: process.env.log4js_logmet_logging_port || config.options && config.options.logging_port,
+        secureProtocol: logmetConnection.DEFAULT_SECURE_PROTOCOL,
+        rejectUnauthorized: logmetConnection.DEFAULT_REJECT_UNAUTHORIZED
     };
 
     var optionsInvalid = false;
@@ -82,6 +308,6 @@ function configure(config) {
     const level = log4js.levels[config.options.level] && log4js.levels[config.options.level].level || Number.MIN_VALUE;
 
     util.log('Logmet appender configured');
-    return appender(level, options);
+    return appender(level, options, tlsOpts);
 };
 
