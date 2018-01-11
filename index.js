6f29c54564daa56b462128568adb8934a94054a5
Dogukan Erenel <derenel@us.ibm.com>
2018-01-10 18:00:26 -0600

    Add support for custom fields and custom field names

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ module.exports = {
     shutdown: shutdown
 };
 
+const messageSeperator = " | ";
+const defaultEventMap = {
+    'component': 'component',
+    'host-ip':  'host-ip',
+    'instance-id':'instance-id',
+    'loglevel': 'loglevel',
+    'logtime': 'logtime',
+    'message': 'message'
+};
+
 function sendData(level, options, tlsOpts, log) {
     var event = buildEvent(log, options);
     logmetConnection.producer.sendData(event, log.categoryName, options.space_id, function(/*error, status*/) {
@@ -28,18 +38,64 @@ function sendData(level, options, tlsOpts, log) {
 
 function buildEvent(log, options) {
     // build up message to send
-    var logData = log.data && log.data.join(' | ');
-
-    var event = {
-        'component': options.component,
-        'host-ip': process.env.CF_INSTANCE_IP,
-        'instance-id': process.env.CF_INSTANCE_INDEX,
-        'loglevel': log.level && log.level.levelStr,
-        'logtime': log.startTime && log.startTime.toISOString(),
-        'message': logData
-    };
-
+    var event = {};
+    if(options && options.event_map){
+        for (var key in options.event_map) {
+            if (!options.event_map.hasOwnProperty(key)) continue;
+            var value = options.event_map[key];
+            event[key] = getValueForMapping(value, options, log);
+        }
+    }
     return event;
+}
+
+function getValueForMapping(key, options, log){
+    var valueMapped;
+    if(key === 'component'){
+        valueMapped = options.component;
+    } else if(key === 'host-ip'){
+        valueMapped = process.env.CF_INSTANCE_IP;
+    } else if(key === 'instance-id'){
+        valueMapped = process.env.CF_INSTANCE_INDEX;
+    } else if(key === 'loglevel'){
+        valueMapped = log.level && log.level.levelStr;
+    } else if(key === 'logtime'){
+        valueMapped = log.startTime && log.startTime.toISOString();
+    } else if(key === 'message'){
+        valueMapped = log.data && log.data.reduce(function (a, b) {
+            return ((typeof(a)==="object" && JSON.stringify(a)) || a) + messageSeperator + ((typeof(b)==="object" && JSON.stringify(b)) || b);
+        });
+    } else if(key.startsWith('process.') && key.length > 8){
+        var processValue = key.substring(8).split('.');
+        if(processValue.length == 1){
+            valueMapped = process[processValue[0]];
+        } else if (processValue.length == 2){ 
+            valueMapped = process[processValue[0]] && process[processValue[0]][processValue[1]];
+        }
+        else{
+            //do nothing - currently only support for 2 layer process information
+        }
+    } else if(key.startsWith('data.') && key.length > 5){ //Custom data parsing using log data array's first `object` type element
+        var logDataObject = log.data && log.data.find(function(element) {
+            return (typeof(element)==="object");
+        });
+        if(logDataObject){
+            var dataValue = key.substring(5).split('.');
+            if(dataValue.length == 1){
+                valueMapped = logDataObject[dataValue[0]];
+            } else if (dataValue.length == 2){ 
+                valueMapped = logDataObject[dataValue[0]] && logDataObject[dataValue[0]][dataValue[1]];
+            } else {
+                 //do nothing - currently only support for 2 layer data information
+            }
+        }
+        else{
+            //do nothing - there is no data object
+        }
+    } else {
+        // No valid mapping found for given key.
+    }
+    return valueMapped;
 }
 
 function appender(level, options, tlsOpts) {
@@ -60,7 +116,8 @@ function configure(config) {
     const options = {
         component: process.env.log4js_logmet_component || config.options && config.options.component,
         logging_token: process.env.log4js_logmet_logging_token || config.options && config.options.logging_token,
-        space_id: process.env.log4js_logmet_space_id || config.options && config.options.space_id
+        space_id: process.env.log4js_logmet_space_id || config.options && config.options.space_id,
+        event_map: (config.options && config.options.eventMap) || defaultEventMap
     };
 
     const tlsOpts = {
