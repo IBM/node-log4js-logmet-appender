b5d5d3ff53feb395e6f49fe1f4c8aca2e29224dc
Simon Kotwicz <simonkotwicz@gmail.com>
2018-03-13 21:58:15 -0400

    Require logs4js 2.x as a peer dependency, support custom layouts

--- README.md
+++ README.md
@@ -19,63 +19,41 @@ export log4js_logmet_logging_port=9091
 To get the secure token and space id see the instructions here: https://pages.github.ibm.com/alchemy-logmet/getting-started/authentication.html
 
 ## Usage
- You must be using [log4js-node](https://github.com/nomiddlename/log4js-node) and must call `log4js.configure('/path/to/log4js.json')`
+ You must be using [log4js-node](https://github.com/nomiddlename/log4js-node) 2.x and must call `log4js.configure('/path/to/log4js.json')`
 somewhere in your code.
 
 You must add the following to the list of appenders in your `log4js.json` file:
 
 ```
 {
-  "type": "log4js-logmet-appender"
+    "appenders": {
+        "logmet": {
+            "type": "log4js-logmet-appender",
+        }
+    },
+    "categories": {
+        "default": { "appenders": [ "logmet" ], "level": "info" },
+    }
 }
  ```
 
-Using this configuration log messages to logmet will contain the following fields:
-- `component`: value of the environment variable `log4js_logmet_component`
-- `host-ip`: value of the environmental variable `CF_INSTANCE_IP`
-- `instance-id`: value of the environmental variable `CF_INSTANCE_INDEX`
-- `loglevel`: logging level (eg. `INFO`)
-- `logtime`: time message was logged in ISO string format (eg. `2011-10-05T14:48:00.000Z`)
-- `message`: log message/data, data array elements are concatenated with ` | ` delimeter
+The appender supports custom layouts, the default layout function used is:
 
-### Custom Fields
-
-To use custom fields, you should define an `options.fields` object.
-Note: If a `fields` object is defined, none of the default fields will be sent.
-
-You can choose names for the log level (`"level_field_name"`), timestamp (`"timestamp_field_name"`) and data/message (`"data_field_name"`) fields. You may also add more custom static fields (under the `"static_fields"` object), fields that have node `process` object properties as values (under the `"process_fields"` object), and fields that have environment variables as values (under the `"env_fields"` object).
-
-Example with custom fields:
 ```
-{ 
-  "type": "log4js-logmet-appender",
-  "options": {
-    "fields": {
-      "level_field_name": "PRIORITY",
-      "timestamp_field_name": "TIMESTAMP",
-      "data_field_name": "MESSAGE"
-
-      "static_fields": {
-        "COMPONENT": "otc-api"
-      },
-
-      "process_fields": {
-        "PROCESS_ID": "pid"
-      },
-
-      "env_fields": {
-        "INSTANCE_INDEX": "CF_INSTANCE_INDEX"
-      }
-    }
-  }
-}
+let layout = (logEvent) => {
+    return {
+        'component': process.env.log4js_logmet_component || config.component,
+        'host-ip': process.env.CF_INSTANCE_IP,
+        'instance-id': process.env.CF_INSTANCE_INDEX,
+        'loglevel': logEvent.level.levelStr,
+        'msg_timestamp': logEvent.startTime.toISOString(),
+        'message': logEvent.data.join(' | '),
+        'type': logEvent.categoryName
+    };
+};
 ```
 
-You may also define custom dynamic fields by setting `options.fields` `"data_field_augment"` to `true` and passing a single object argument to the logger. Object keys will be mapped to field names.
-
-Example: `logger.info({ MESSAGE: "hello", FLAG: "urgent" });`
-
-Note: If `"data_field_augment"` is set to `true` and a single object argument is NOT passed to the logger, the log message/data will still be sent as a regular field if `"data_field_name"` is also set.
+Each key will mapped to a field name in Logmet.
 
 ## Pending work
 No automated tests currently
--- index.js
+++ index.js
@@ -1,131 +1,79 @@
 /**
  * Licensed Materials - Property of IBM
- * (c) Copyright IBM Corporation 2016, 2017. All Rights Reserved.
+ * (c) Copyright IBM Corporation 2016, 2018. All Rights Reserved.
  *
  * Note to U.S. Government Users Restricted Rights:
  * Use, duplication or disclosure restricted by GSA ADP Schedule
  * Contract with IBM Corp.
  */
+
 'use strict';
 
-var log4js = require('log4js'),
-    util = require('util'),
-    logmetConnection = require('./lib/logmet-connection-singleton'),
-    logmet = require('logmet-client');
+const LogmetProducer = require('logmet-client').LogmetProducer;
 
-module.exports = {
-    appender: appender,
-    configure: configure,
-    shutdown: shutdown
-};
+function logmetAppender(logmetClientProducer, spaceId, layout, timezoneOffset) {
 
-function sendData(options, tlsOpts, log) {
-    var event = buildEvent(log, options);
-    logmetConnection.producer.sendData(event, log.categoryName, options.space_id, function(/*error, status*/) {
-        // message is dropped if an error is returned, errors already logged by logmet client
-    });
-}
+    const appender = (loggingEvent) => {
+        logmetClientProducer.sendData(layout(loggingEvent, timezoneOffset), spaceId);
+    };
 
-function buildEvent(log, options) {
-    // build up message to send
-    let event = {};
-    let fields = options.fields;
-
-    if (fields.level_field_name) { 
-        event[fields.level_field_name] = log.level && log.level.levelStr; 
-    }
-    if (fields.timestamp_field_name) { 
-        event[fields.timestamp_field_name] = log.startTime && log.startTime.toISOString();
-    }
-    if (fields.data_field_augment && log.data && log.data.length === 1 && typeof log.data[0] === 'object') {
-        let customDynamicFields = log.data[0];
-        for (let customDynamicFieldName in customDynamicFields) {
-            event[customDynamicFieldName] = customDynamicFields[customDynamicFieldName];
+    appender.shutdown = (callback) => {
+        if (logmetClientProducer) {
+            logmetClientProducer.terminate(callback);
         }
-    } else if (fields.data_field_name) {
-        event[fields.data_field_name] = log.data && log.data.join(' | ');
-    }
-    for (let staticFieldName in fields.static_fields) {
-        event[staticFieldName] = fields.static_fields[staticFieldName];
-    }
-    for (let processFieldName in fields.process_fields) {
-        event[processFieldName] = process[fields.process_fields[processFieldName]];
-    }
-    for (let envFieldName in fields.env_fields) {
-        event[envFieldName] = process.env[fields.env_fields[envFieldName]];
-    }
-
-    return event;
+        else {
+            callback();
+        }
+    };
+    return appender;
 }
 
-function appender(options, tlsOpts) {
-    logmetConnection.producer = new logmet.LogmetProducer(tlsOpts.host, tlsOpts.port, options.space_id, options.logging_token, false, {bufferSize: logmetConnection.BUFFER_SIZE});
-    logmetConnection.producer.connect(function(error, status) {
+function configure(config, layouts) {
+    if (process.env.log4js_logmet_enabled !== 'true') { return () => {}; }
+
+    const options = {};
+
+    options.host = process.env.log4js_logmet_logging_host || config.logging_host;
+    options.port = process.env.log4js_logmet_logging_port || config.logging_port;
+    options.spaceId = process.env.log4js_logmet_space_id || config.space_id;
+    options.loggingToken = process.env.log4js_logmet_logging_token || config.logging_token;
+
+    for (let option in options) {
+        if (!options[option]) {
+            console.log(`Logmet appender: ${ option } not specified`);
+            return () => {};
+        }
+    }
+
+    let layout = (logEvent) => {
+        return {
+            'component': process.env.log4js_logmet_component || config.component,
+            'host-ip': process.env.CF_INSTANCE_IP,
+            'instance-id': process.env.CF_INSTANCE_INDEX,
+            'loglevel': logEvent.level.levelStr,
+            'msg_timestamp': logEvent.startTime.toISOString(),
+            'message': logEvent.data.join(' | '),
+            'type': logEvent.categoryName
+        };
+    };
+    if (config.layout) {
+        layout = layouts.layout(config.layout.type, config.layout);
+    }
+
+    console.log('Logmet appender configured');
+
+    const logmetClientProducer = new LogmetProducer(options.host, options.port, options.spaceId, options.loggingToken, false, 
+        {bufferSize: process.env.log4js_logmet_buffer_size || 10000});
+
+    logmetClientProducer.connect((error, status) => {
         if (error) {
-            util.log('Logmet Appender: Connection with Logmet failed. ERROR: ' + error);
+            console.error(`Logmet appender: Connection with Logmet failed. Details: ${ error }`);
         } else if (status.handshakeCompleted) {
-            util.log('Logmet Appender: LogmetClient is ready to send data.'); 
+            console.log('Logmet appender: LogmetClient is ready to send data.'); 
         }
     });
-    return sendData.bind(this, options, tlsOpts);
+    return logmetAppender(logmetClientProducer, options.spaceId, layout, config.timezoneOffset);
 }
 
-function configure(config) {
-    if (process.env.log4js_logmet_enabled !== 'true') return function() {};
-
-    const defaultFields = {
-        'level_field_name': 'loglevel',
-        'timestamp_field_name': 'logtime',
-        'data_field_name': 'message',
-
-        'static_fields': {
-            'component': process.env.log4js_logmet_component || config.options && config.options.component
-        },
-        'env_fields': {
-            'host-ip': 'CF_INSTANCE_IP',
-            'instance-id': 'CF_INSTANCE_INDEX'
-        }
-    };
-
-    const options = {
-        logging_token: process.env.log4js_logmet_logging_token || config.options && config.options.logging_token,
-        space_id: process.env.log4js_logmet_space_id || config.options && config.options.space_id,
-        fields: (config.options && config.options.fields) || defaultFields
-    };
-
-    const tlsOpts = {
-        host: process.env.log4js_logmet_logging_host || config.options && config.options.logging_host,
-        port: process.env.log4js_logmet_logging_port || config.options && config.options.logging_port,
-        secureProtocol: logmetConnection.DEFAULT_SECURE_PROTOCOL,
-        rejectUnauthorized: logmetConnection.DEFAULT_REJECT_UNAUTHORIZED
-    };
-
-    var optionsInvalid = false;
-
-    for (var i in options) {
-        if (!options[i]) {
-            util.log('Logmet Appender: ' + i + ' not specified');
-            optionsInvalid = true;
-        }
-    }
-
-    if (optionsInvalid) return function() {};
-
-    if (config.appender) {
-        log4js.loadAppender(config.appender.type);
-        config.actualAppender = log4js.appenderMakers[config.appender.type](config.appender);
-    }
-
-    util.log('Logmet Appender configured');
-    return appender(options, tlsOpts);
-}
-
-function shutdown(callback) {
-    if (logmetConnection.producer) {
-        logmetConnection.producer.terminate(callback);
-    }
-    else {
-        callback();
-    }
-}
+module.exports.configure = configure;
 
